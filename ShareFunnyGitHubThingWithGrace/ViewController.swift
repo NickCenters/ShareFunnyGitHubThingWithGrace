56f43a2f813460af167a6fb13ac225e93d764a63
Nick <117998@swcsd.us>
2021-03-22 14:38:37 -0400

    This do bw a test du

--- ShareFunnyGitHubThingWithGrace/ViewController.swift
+++ ShareFunnyGitHubThingWithGrace/ViewController.swift
@@ -1,18 +1,11 @@
-//
-//  ViewController.swift
-//  ShareFunnyGitHubThingWithGrace
-//
-//  Created by Nicholas Centers on 3/22/21.
-//  Copyright © 2021 Nicholas Centers. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+//This do be a test
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
